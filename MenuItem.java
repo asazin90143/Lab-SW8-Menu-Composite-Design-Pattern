fb7b2cdf66a6d9de52c65035c0f92b585001b1c8
KENNETH BRYAN GERABAS ESCALA <69161486+asazin90143@users.noreply.github.com>
2026-04-21 02:34:34 +0000

    MenuItem.java

---
+++ MenuItem.java
@@ -0,0 +1,23 @@
+/**
+ * Leaf Class
+ * Represents individual menu items that do not contain other items.
+ */
+public class MenuItem implements MenuComponent {
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("  " + name + ": P" + String.format("%.2f", price));
+    }
+}
